5c04f457841a7983e3cb088919eaee2b91b7caab
laketaupo <stefanbakker02@gmail.com>
2026-06-04 11:39:15 +0000

    commit astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  // Update site and base when you know your GitHub Pages URL:
-  // site: 'https://YOUR-USERNAME.github.io',
-  // base: '/YOUR-REPO-NAME',
+  site: 'https://laketaupo.github.io',
+  base: '/Development',
   integrations: [tailwind(), mdx()],
 });
