e942c80e5880c83f326edc609b6fcc6a1ca55040
Stefan e <stefanbakker02@gmail.com>
2026-06-07 19:16:26 +0200

    Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://stefanbakker.com',
   base: '/',
+  adapter: vercel(),
   integrations: [tailwind(), mdx()],
 });
