02a5b20b36d894c0091a8d4c1c097efa69280f6f
laketaupo <stefanbakker02@gmail.com>
2026-06-24 15:39:45 +0200

    Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://stefanbakker.com',
   base: '/',
-  adapter: vercel(),
+  output: 'static',
   integrations: [tailwind(), mdx()],
   devToolbar: { enabled: false },
 });
